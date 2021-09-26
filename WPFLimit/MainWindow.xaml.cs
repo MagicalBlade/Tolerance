bf1867423f13c149f3f7c60f6bcc3c906f8a5555
Александр Воскобойник <magical.blade@gmail.com>
2021-09-26 18:38:13 +0300

    добавлена история. добавлено сохранение

--- WPFLimit/MainWindow.xaml.cs
+++ WPFLimit/MainWindow.xaml.cs
@@ -18,6 +18,9 @@ using System.Collections;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Xml;
 
 namespace WPFLimit
 {
@@ -28,20 +31,34 @@ namespace WPFLimit
     {
         TSM.Model model;
         TSD.DrawingHandler drawingHandler;
-        
-        
+        List<String> save = new List<string>();
         public MainWindow()
         {
+            List<string> save = new List<string>();
             InitializeComponent();
-            load();
+            Load();
         }
 
-        private void load()
+        private void Load()
         {
             XDocument xdoc = XDocument.Load("save.xml");
             w_main.Topmost = (bool)xdoc.Element("setting").Element("Поверх_окон");
             w_main.Left = (double)xdoc.Element("setting").Element("Лево");
             w_main.Top = (double)xdoc.Element("setting").Element("Верх");
+
+            XmlSerializer formatter = new XmlSerializer(typeof(List<string>));
+            List<string> f_temp = (List<string>)formatter.Deserialize(xdoc.Element("setting").Element("ArrayOfString").CreateReader());
+            save = f_temp;
+            //using (FileStream fs = new FileStream("save.xml", FileMode.OpenOrCreate))
+            //{
+            //    List<string> d_temp = (List<string>)formatter.Deserialize(fs);
+            //    save = d_temp;
+            //}
+            foreach (var item in save)
+            {
+                lb_save.Items.Add(StackPanel(item, b_delete_Click));
+            }
+
 
             if (w_main.Topmost)
             {
@@ -95,15 +112,30 @@ namespace WPFLimit
                 MessageBox.Show("Чертеж не запущен :(");
                 return;
             }
+            StraightDimension();
 
+            if (lb_history.Items.Count == 0)
+            {
+                lb_history.Items.Add(StackPanel(tb_limit_up.Text + "; " + tb_limit_down.Text, b_save_Click));
+            }
+            else
+            {
+                if (lb_history.Items[lb_history.Items.Count - 1].ToString() != (tb_limit_up.Text + "; " + tb_limit_down.Text))
+                {
+                    lb_history.Items.Add(StackPanel(tb_limit_up.Text + "; " + tb_limit_down.Text, b_save_Click));
+                }
+            }
+        }
+
+        private void StraightDimension()
+        {
             TSD.Drawing drawing = drawingHandler.GetActiveDrawing();
             foreach (TSD.DrawingObject drawingObject in drawingHandler.GetDrawingObjectSelector().GetSelected())
             {
-                //MessageBox.Show(drawingObject.GetType().ToString());
                 if (drawingObject is TSD.StraightDimension)
                 {
                     TSD.StraightDimension straightDimension = drawingObject as TSD.StraightDimension;
-                    Prefix(straightDimension, drawing);
+                    Prefix(straightDimension);
                 }
 
                 if (drawingObject is TSD.StraightDimensionSet)
@@ -114,13 +146,13 @@ namespace WPFLimit
                         if (drawingObjectEnumerator.Current is TSD.StraightDimension)
                         {
                             TSD.StraightDimension straightDimension = drawingObjectEnumerator.Current as TSD.StraightDimension;
-                            Prefix(straightDimension, drawing);
+                            Prefix(straightDimension);
                         }
                 }
             }
             drawing.CommitChanges();
         }
-       private void Prefix(TSD.StraightDimension straightDimension, TSD.Drawing drawing)
+       private void Prefix(TSD.StraightDimension straightDimension)
         {
             TSD.ContainerElement containerElement = new TSD.ContainerElement();
             TSD.ContainerElement cE_return = new TSD.ContainerElement();
@@ -180,6 +212,22 @@ namespace WPFLimit
             straightDimension.Modify();
         }
 
+        private StackPanel StackPanel(string text, RoutedEventHandler routedEventHandler)
+        {
+            Button b_save = new Button();
+            Label l_save = new Label();
+            l_save.Content = text;
+            b_save.Height = 15;
+            b_save.Width = 15;
+            b_save.Click += routedEventHandler;
+            b_save.Tag = text;
+            StackPanel stackPanel = new StackPanel();
+            stackPanel.Orientation = Orientation.Horizontal;
+            stackPanel.Children.Add(b_save);
+            stackPanel.Children.Add(l_save);
+            return stackPanel;
+        }
+
         private void tb_limit_up_GotFocus(object sender, RoutedEventArgs e)
         {
             tb_limit_up.SelectAll();
@@ -224,7 +272,7 @@ namespace WPFLimit
             this.DragMove();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void b_Topmost_Click(object sender, RoutedEventArgs e)
         {
             if (!w_main.Topmost)
             {
@@ -240,23 +288,46 @@ namespace WPFLimit
 
         private void w_main_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            //var options = new JsonSerializerOptions
-            //{
-            //    WriteIndented = true,
-            //};
-            //ArrayList json_save = new ArrayList()
-            //{
-            //    w_main.Topmost,
-            //    w_main.Left,
-            //    w_main.Top
-            //};
-            //File.WriteAllText("user.json", JsonSerializer.Serialize(json_save, options));
-
-            XDocument xdoc = new XDocument(new XElement("setting",
+            XDocument xdoc = new XDocument();
+            xdoc.Add(new XElement("setting",
                 new XElement("Поверх_окон", w_main.Topmost),
                 new XElement("Лево", w_main.Left),
                 new XElement("Верх", w_main.Top)));
+            XmlSerializer formatter = new XmlSerializer(typeof(List<string>));
+            XDocument xdoc1 = new XDocument();
+            using (XmlWriter fs = xdoc1.CreateWriter())
+            {
+                formatter.Serialize(fs, save);
+            }
+            xdoc.Root.Add(xdoc1.Root);
             xdoc.Save("save.xml");
+
+        }
+
+        private void lb_history_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            StackPanel stackPanel = lb_history.SelectedItem as StackPanel;
+            Label label = stackPanel.Children[1] as Label;
+            string[] temp = label.Content.ToString().Split(';');
+            tb_limit_up.Text = temp[0].Trim();
+            tb_limit_down.Text = temp[1].Trim();
+            StraightDimension();
+        }
+
+        private void b_save_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            lb_save.Items.Add(StackPanel(button.Tag.ToString(), b_delete_Click));
+            save.Add(button.Tag.ToString());
+        }
+        private void b_delete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("delete");
+        }
+
+        private void lb_save_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
         }
     }
 }
